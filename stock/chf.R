48baf6adc1d69dd1eda36ed5518062eaf51c5a16
CHU MAI <e57677@dsp0722636.postes.calibre.edf.fr>
2018-12-21 16:47:40 +0100

    update chf

--- stock/chf.R
+++ stock/chf.R
@@ -12,6 +12,17 @@ if (!require("magrittr")) {
 }
 
 start <- as.Date("2016-01-01")
-end <- as.Date("2018-12-01")
+end <- as.Date("2018-12-20")
 
 getFX("CHF/EUR", src = "yahoo", from = start, to = end)
+
+class(CHFEUR)
+
+head(CHFEUR)
+
+plot(CHFEUR[, "CHF.EUR"], main = "CHF/EUR")
+
+candleChart(CHFEUR)
+addSMA(n = 10, col = 'red')
+addSMA(n = 20, col = 'blue')
+addSMA(n = 30, col = 'violet')
